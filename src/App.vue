eb245f55a321a853cc530a852db688470d132cf5
Akshat Jain <u7284072@anu.edu.au>
2022-06-11 22:44:13 +1000

    Removed Sample App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
     <main>
-      <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather"/>
+      <div class="introduction">
+        <div class="title">
+          Hi! My name is Akshat Jain
+        </div>
+        <div class="description">
+          I am a Student at the Australian National University (ANU) and a Database Engineer at Services Australia
+        </div>
       </div>
 
-      <div class="weather=wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="location-box">
-          <div class="location">
-            {{ weather.name }}, {{ weather.sys.country }}
-          </div>
-          <div class="date">
-            {{ dateBuilder() }}
-          </div>
-
-          <div class="weather-box">
-            <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-            <div class="weather">{{ weather.weather[0].main }}</div>
-          </div>
+      <div class="contact">
+        <div class="github">
+          <button type="button" id="gitbutton">Github</button>
         </div>
+
+        <div class="resume">
+
+        </div>
+
+        <div class="linkedin">
+
+
+        </div>
+
+      </div>
+
+      <div class="work">
+
+      </div>
+
+      <div class="projects">
+
+      </div>
+
+      <div class="university">
+
       </div>
     </main>
   </div>
@@ -32,20 +49,17 @@
       return {
         weather_api: '08d19d2cfc7606d7d78082f4185a934b',
         url_base: 'http://api.openweathermap.org/data/2.5/',
-        query:'',
-        weather: {
-
-        }
+        query:'Canberra',
+        weather: {}
       };
     },
+
     methods: {
-      fetchWeather(e) {
-        if (e.key == "Enter") {
-          fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.weather_api}`)
+      fetchWeather() {
+          fetch(`${this.url_base}weather?q=Canberra&units=metric&APPID=${this.weather_api}`)
           .then(res => {
             return res.json();
           }).then(this.setResults);
-        }
       },
       setResults (results) {
           this.weather = results;
@@ -60,7 +74,10 @@
       let month = months[d.getMonth()];
       let year = d.getFullYear();
       return `${day} ${date} ${month} ${year}`;
-    }
+      }
+    },
+    beforeMount() {
+      this.fetchWeather()
     }
   }
 </script>
@@ -79,7 +96,6 @@ body {
 }
 
 #app {
-  background-image: url('./assets/cold-bg.jpg');
   background-size:contain;
   background-position: bottom;
   transition: 0.4s;
@@ -88,74 +104,27 @@ body {
 main {
   min-height: 200vh;
   padding: 25px;
-
-  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+  background-color: black;
 }
 
-.search-box {
-  width: 100%;
-  margin-bottom: 30px;
+.introduction {
+  padding: 25px;
+  text-align: center;
+  font-family: PortfolioFont;
+  font-weight: 100;
+  color: #FFF;
 }
 
-.search-box .search-bar {
-  display: block;
-  width: 100%;
-  padding: 15px;
+.introduction .title {
+  font-size: 50px;
+}
 
-  color:#313131;
+.introduction .description {
   font-size: 20px;
-
-  appearance: none;
-  border: none;
-  outline: none;
-  background: none;
-
-  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255,255,255,0.5);
-  border-radius: 0px 16px 0px 16px;
-  transition: 0.4s;
 }
 
-.search-box .search-bar:focus {
-  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
-  background-color: rgba(255,255,255,0.75);
-  border-radius: 16px 0px 16px 0px;
-}
-
-.location-box .location {
-  color: #FFF;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-}
-.location-box .date {
-  color: #FFF;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
-}
-.weather-box {
-  text-align: center;
-}
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #FFF;
-  font-size: 102px;
-  font-weight: 900;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color:rgba(255, 255, 255, 0.25);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-.weather-box .weather {
-  color: #FFF;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+@font-face {
+  font-family: PortfolioFont;
+  src: url("./assets/fonts/LibreFranklin-VariableFont_wght.ttf");
 }
 </style>
