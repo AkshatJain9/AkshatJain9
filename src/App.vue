7c64c244bc8dec03e4b98cb62e3159d27bc94063
Akshat Jain <u7284072@anu.edu.au>
2022-07-11 11:58:46 +1000

    Fixed Background Interactivity

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div id="app">
     <main>
-      
-      
-    
       <Introduction />
       <Contact />
       <Skills />
-
 
       <div class="projects">
         <div class="projectstitle">Software Projects</div>
@@ -23,46 +19,134 @@
 
 <script>
 
-
 import Timeline from './components/Timeline.vue';
 import Introduction from './components/Introduction.vue';
 import Contact from './components/Contact.vue';
 import Skills from './components/Skills.vue'; 
 
-particlesJS("particles-js", 
-          {"particles":{"number":{"value":85,
-          "density":{"enable":true,"value_area":800}},
-          "color":{"value":"#ffffff"},
-          "shape":{"type":"polygon","stroke":{"width":0,"color":"#000000"},
-          "polygon":{"nb_sides":11},
-          "image":{"src":"img/github.svg","width":100,"height":100}},
-          "opacity":{"value":1,"random":false,
-          "anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
-          "size":{"value":1,"random":true,
-          "anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
-          "line_linked":{"enable":true,"distance":160.3412060865523,
-          "color":"#ffffff","opacity":0.44093831673801875,
-          "width":1.2827296486924182},
-          "move":{"enable":true,"speed":6,"direction":"top-right",
-          "random":false,"straight":false,"out_mode":"out","bounce":false,
-          "attract":{"enable":false,"rotateX":3367.1653278175977,
-          "rotateY":2324.947488255008}}},"interactivity":{"detect_on":"canvas",
-          "events":{"onhover":{"enable":true,"mode":"grab"},
-          "onclick":{"enable":true,"mode":"repulse"},"resize":true},
-          "modes":{"grab":{"distance":231.44200550588337,"line_linked":{"opacity":1}},
-          "bubble":{"distance":400,"size":40,"duration":0.48724632738080703,
-          "opacity":0.7146279468251837,"speed":3},"repulse":{"distance":200,"duration":0.4},
-          "push":{"particles_nb":4},"remove":{"particles_nb":2}}},
-          "retina_detect":true});
-          var count_particles, stats, update; stats = new Stats; 
-          stats.setMode(0); stats.domElement.style.position = 'absolute'; 
-          stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; 
-          document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); 
-          update = function() { stats.begin(); stats.end(); 
-          if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) 
-            { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } 
-          requestAnimationFrame(update); }; 
-          requestAnimationFrame(update);;
+particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value":85,
+          "density": {
+            "enable":true,
+            "value_area":800
+          }
+        },
+        "color": {
+          "value":"#ffffff"
+        },
+        "shape": {
+          "type":"polygon",
+          "stroke":{
+            "width":0,
+            "color":"#000000"
+          },
+          "polygon":{
+            "nb_sides":11
+          },
+          "image":{
+            "src":"img/github.svg",
+            "width":100,
+            "height":100
+          }
+        },
+        "opacity":{
+          "value":1,
+          "random":false,
+          "anim":{
+            "enable":false,
+            "speed":1,
+            "opacity_min":0.1,
+            "sync":false
+          }
+        },
+        "size":{
+          "value":1,
+          "random":true,
+          "anim":{
+            "enable":false,
+            "speed":40,
+            "size_min":0.1,
+            "sync":false
+          }
+        },
+        "line_linked":{
+          "enable":true,
+          "distance":160.3412060865523,
+          "color":"#ffffff",
+          "opacity":0.44093831673801875,
+          "width":1.2827296486924182
+        },
+        "move":{
+          "enable":true,
+          "speed":6,
+          "direction":"top-right",
+          "random":false,
+          "straight":false,
+          "out_mode":"out",
+          "bounce":false,
+          "attract":{
+            "enable":false,
+            "rotateX":3367.1653278175977,
+            "rotateY":2324.947488255008
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on":"window",
+        "events": {
+          "onhover": {
+            "enable":true,
+            "mode":"grab"
+          },
+          "onclick":{
+            "enable":true,
+            "mode":"repulse"
+          },
+          "resize":true
+        },
+        "modes":{
+          "grab":{
+            "distance":231.44200550588337,
+            "line_linked":{
+              "opacity":1
+            }
+          },
+          "bubble":{
+            "distance":400,
+            "size":40,
+            "duration":0.48724632738080703,
+            "opacity":0.7146279468251837,"speed":3
+          },
+          "repulse":{
+            "distance":200,
+            "duration":0.4
+          },
+          "push":{
+            "particles_nb":4
+          },
+          "remove":{
+            "particles_nb":2
+          }
+        }
+      },
+      "retina_detect":true
+    });
+
+    var count_particles, stats, update; stats = new Stats; 
+    stats.setMode(0); stats.domElement.style.position = 'absolute'; 
+    stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; 
+    document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); 
+    update = function() { 
+      stats.begin(); 
+      stats.end(); 
+      if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; 
+        } 
+      requestAnimationFrame(update); 
+      }; 
+    requestAnimationFrame(update);
 
   export default {
     name: 'app',
@@ -88,16 +172,8 @@ particlesJS("particles-js",
 
 
 <style>
-/* #background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: -100;
-} */
 
-canvas{ display: block;  } /* ---- particles.js container ---- */ 
+canvas{display: block;}
 #particles-js { 
   position:absolute; 
   width: 100%; 
