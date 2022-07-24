a8b14490ea031e8a7b8241dc59a45e12520b047d
Akshat Jain <u7284072@anu.edu.au>
2022-07-24 12:37:52 +1000

    Testing new batch file

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,5 @@
 call npm run build
 git add dist -f
-git commit -m "Deploying Updates to Github Pages"
+git commit -m "Deploying Updates to Github Pages $(Get-Date -format 'd')"
 git subtree push --prefix dist origin gh-pages
+git push
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,28 +1,21 @@
  <template>
     <div id="contact">
     <div class="contact">
-      <div class="github">
+
         <div class="githubbackground" onclick="window.open('https://github.com/AkshatJain9', '_blank')">
           <button class="githubbutton" type="button" id="gitbutton" onclick="window.open('https://github.com/AkshatJain9', '_blank'); window.event.cancelBubble = true"></button>
           <div class="githubtext">GitHub</div>
         </div>
-      </div>
 
-      <div class ="resume">
         <div class="resumebackground" onclick="window.open('https://akshatjain9.github.io/Resume/Resume_JAIN.pdf', '_blank')">
-        <button class="resumebutton" type="button" id="resbutton" onclick="window.open('https://akshatjain9.github.io/Resume/Resume_JAIN.pdf', '_blank'); window.event.cancelBubble = true"></button>
-
+          <button class="resumebutton" type="button" id="resbutton" onclick="window.open('https://akshatjain9.github.io/Resume/Resume_JAIN.pdf', '_blank'); window.event.cancelBubble = true"></button>
           <div class="resumetext">Resume</div>
         </div>
-      </div>
 
-      <div class="linkedin">
         <div class="linkedinbackground" onclick="window.open('https://www.linkedin.com/in/akshat-jain-155b74203/', '_blank')">
           <button class="linkedinbutton" type="button" id="libutton" onclick="window.open('https://www.linkedin.com/in/akshat-jain-155b74203/', '_blank'); window.event.cancelBubble = true"></button>
           <div class="linkedintext">LinkedIn</div>
         </div>
-        
-      </div>
   </div>
   </div>
 </template>
