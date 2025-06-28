313f2b537f027cd4c2cb6ac4534cda19dee91c6c
AkshatJain9 <akshatt.jain9@gmail.com>
2025-06-28 17:55:49 +1000

    Basic Refactor Started

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,6 +3,11 @@
     <div class="skillstitle">Skills at a Glance</div>
       <div class="skills">
         <div class="img_wrapper">
+          <div class="text"> Java is my strongest programming language as I have built several applications and tools using it, including Android Applications. I have knowledge
+            of the standard library as well as frameworks such as Spring Boot</div>
+          <img src="../assets/backend.png" alt="java" width="120" height="120">
+        </div>
+        <div class="img_wrapper">
           <div class="text"> Having majored in Machine Learning, I am proficient in Python and its core libraries including 
           NumPy, Pandas, MPL, PyTorch and more. I also have experience in building Python backends 
           for web applications using Flask </div>
@@ -14,24 +19,7 @@
             HTTP Proxy listed below. I have also experimented with common libraries such as SDL
           </div>
           <img src="../assets/cpp.png" alt="cpp" width="110" height="120">
-        </div> 
-        <div class="img_wrapper">
-          <div class="text"> I have worked with common tools such as Git and have experience in using Cloud Services on platforms such as Azure. 
-            I have experience in using DevOps principles having created CI/CD pipelines and also used technologies such as Docker
-          </div>
-          <img src="../assets/git.png" alt="git" width="120" height="120">
-        </div> 
-        <div class="img_wrapper">
-          <div class="text"> Java is my strongest programming language as I have built several applications and tools using it, including Android Applications. I have knowledge
-            of the standard library as well as frameworks such as Spring Boot</div>
-          <img src="../assets/java.png" alt="java" width="120" height="120">
-        </div> 
-        <div class="img_wrapper">
-          <div class="text"> I have experience creating Web-Apps using Vue, Angular and React. I am capable
-            of writing both JavaScript and TypeScript and enjoy learning new frameworks
-          </div>
-          <img src="../assets/javascript.png" alt="javascript" width="120" height="120">
-        </div> 
+        </div>
       </div>
   </div>
 
