7526b323178fced1426ed6958718ded119c83d07
AkshatJain9 <akshatt.jain9@gmail.com>
2025-12-25 12:01:11 +1100

    Updated deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,4 @@
 call npm run build
 git add dist -f
 git commit -m "Deploying Updates to Github Pages"
-git subtree push --prefix dist origin gh-pages
-git push
+git subtree push --prefix dist origin gh-pages --force
