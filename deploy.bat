44417e881dfede6fdbe294698e56296c6ab504dc
AkshatJain9 <akshatt.jain9@gmail.com>
2026-01-31 15:35:55 +1100

    Temp deploy

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-call npm run build
-git add dist -f
-git commit -m "Temp deploy"
-git subtree split --prefix dist -b gh-pages-temp
-git push origin gh-pages-temp:gh-pages --force
-git branch -D gh-pages-temp
-git rm -r --cached dist
-git commit -m "Remove dist from main branch"
--- server/routes.ts
+++
@@ -1,9 +0,0 @@
-import type { Express } from "express";
-import { createServer, type Server } from "http";
-
-export async function registerRoutes(
-  httpServer: Server,
-  app: Express
-): Promise<Server> {
-  return httpServer;
-}
